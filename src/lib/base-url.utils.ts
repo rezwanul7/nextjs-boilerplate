e54623eb061e17777fa50e0f27ee3361aef525a8
Rezwanul Islam <rezwanul7@gmail.com>
2025-10-01 17:56:08 +0600

    consider production url for production env

--- src/lib/base-url.utils.ts
+++ src/lib/base-url.utils.ts
@@ -1,8 +1,11 @@
 import {getEnvVal} from "@/lib/env.utils";
 
 export const getBaseUrl = () => {
+    const vercelEnv = getEnvVal({name: "VERCEL_ENV"});
     const vercelUrl = getEnvVal({name: "VERCEL_URL"});
-    if (vercelUrl) return `https://${vercelUrl}`;
+    const vercelProductionUrl = getEnvVal({name: "VERCEL_PROJECT_PRODUCTION_URL"});
+
+    if (vercelEnv) return vercelEnv === "production" ? `https://${vercelProductionUrl}` : `https://${vercelUrl}`;
 
     const renderUrl = getEnvVal({name: "RENDER_EXTERNAL_URL"});
     if (renderUrl) return renderUrl;
