3936eb16e06324c774a0457a8fad92abb04115f3
Rezwanul Islam <rezwanul7@gmail.com>
2025-10-07 21:28:12 +0600

    refactor fetch posts

--- src/app/(landing)/posts/_lib/post.api.client.ts
+++ src/app/(landing)/posts/_lib/post.api.client.ts
@@ -1,19 +1,11 @@
 import {envConfig} from "@/config/env";
 
 export async function fetchPosts(queryString?: string) {
-    const isServer = typeof window === "undefined"; // true = server, false = browser
-    const str = isServer ? "SERVER" : "BROWSER";
-
-    const rand = Math.floor(Math.random() * 1000);
-    console.log(`fetchPosts (${str}) (${rand}) - queryString:`, queryString);
-
     const res = await fetch(`${envConfig.baseUrl}/api/posts${queryString}`);
 
     if (!res.ok) {
         throw new Error("Failed to fetch posts");
     }
 
-    const data = await res.json();
-    console.log(`resolved fetchPosts (${str}) (${rand}) - data:`, data);
-    return data;
+    return await res.json();
 }
