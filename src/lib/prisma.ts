49177cb2b19cbd3ae33807b62f16fda7c652f472
Rezwanul Islam <rezwanul7@gmail.com>
2025-10-11 12:25:29 +0600

    update prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import {PrismaClient} from "@prisma/client";
+import {envConfig} from "@/config/env";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
 export const prisma =
     globalForPrisma.prisma ??
-    new PrismaClient({
+    new PrismaClient(envConfig.debug ? {
         log: ["query", "error", "warn"], // optional
-    });
+    } : undefined);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
